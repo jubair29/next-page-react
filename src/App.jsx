0a4038349848650c7dca3e9f47fdead4a081f7e4
jubair29 <jubairjihad00@gmail.com>
2023-08-23 11:05:04 +0600

    navber active complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from './componants/Footer';
+import Header from './componants/Header';
 
 const App = () => {
   return (
     <div>
       <div>This is main part of web site</div>
+      <Header/>
 
       <Outlet></Outlet>
       <Footer></Footer>
--- src/componants/Header.jsx
+++ src/componants/Header.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import { BoltIcon } from '@heroicons/react/24/solid'
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        // this is part of Navber ................
-        <nav>
-            {/* Header Icon part */}
-            <div>
-                <span></span>
-            </div>
-            {/* Header Menu Part */}
-            <div>
 
-            </div>
+        <nav className=' my-container flex justify-between bg-gray-100 px-4 py-5'>
+            {/* Logo section */}
+
+            <Link to='/' className='flex items-center'>
+                <BoltIcon className='w-6 h-6 text-blue-700'/>
+                <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>NextPage</span>
+            </Link>
+
+            {/* nav Items  */}
+
+            <ul className='items-center hidden lg:flex space-x-8 font-semibold text-gray-800'>
+                <li>
+                    <NavLink to='/' className={({isActive}) => (isActive ? 'text-blue-800' : '')}>
+                        Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/book' className={({isActive}) => (isActive ? 'text-blue-800' : '')}>
+                        Books</NavLink>
+
+                </li>
+                <li>
+                    <NavLink to='/about' className={({isActive}) => (isActive ? 'text-blue-800' : '')}>
+                        About Us</NavLink>
+
+                </li>
+
+            </ul>
         </nav>
     );
 };
