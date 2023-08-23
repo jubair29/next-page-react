c4f3b8243f704d10089b3aef3bc4f90085496610
jubair29 <jubairjihad00@gmail.com>
2023-08-23 12:54:05 +0600

    navber complete

--- src/componants/Header.jsx
+++ src/componants/Header.jsx
@@ -1,39 +1,69 @@
-import React from 'react';
-import { BoltIcon } from '@heroicons/react/24/solid'
-import { Link, NavLink } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  BoltIcon,
+  Bars3BottomRightIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
+  const [open, setOpen] = useState(false);
+  return (
+    <nav className=" flex justify-between bg-gray-100 px-4 py-5 mx-auto">
+      {/* Logo section */}
 
-        <nav className=' my-container flex justify-between bg-gray-100 px-4 py-5'>
-            {/* Logo section */}
+      <div>
+        <Link to="/" className="flex items-center">
+          <BoltIcon className="w-6 h-6 text-blue-700" />
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
+            NextPage
+          </span>
+        </Link>
+      </div>
 
-            <Link to='/' className='flex items-center'>
-                <BoltIcon className='w-6 h-6 text-blue-700'/>
-                <span className='ml-2 text-xl font-bold tracking-wide text-gray-800'>NextPage</span>
-            </Link>
-
-            {/* nav Items  */}
-
-            <ul className='items-center hidden lg:flex space-x-8 font-semibold text-gray-800'>
-                <li>
-                    <NavLink to='/' className={({isActive}) => (isActive ? 'text-blue-800' : '')}>
-                        Home</NavLink>
+      {/* nav Items  */}
+      <div>
+            <ul className="items-center  md:flex font-semibold text-gray-800">
+                 <div onClick={() => setOpen(!open)}>
+                    <span>
+                        {
+                            open === true ?
+                            <XMarkIcon className="w-6 h-6 text-blue-700 md:hidden"/> 
+                            : <Bars3BottomRightIcon className="w-6 h-6 text-blue-700 md:hidden"/>
+                           
+                        }
+                    </span>
+                 </div>
+                <li className="md:ml-6">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => (isActive ? "text-blue-800" : "")}
+                >
+                    Home
+                </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/book' className={({isActive}) => (isActive ? 'text-blue-800' : '')}>
-                        Books</NavLink>
-
+                <li className="md:ml-6">
+                <NavLink
+                    to="/book"
+                    className={({ isActive }) => (isActive ? "text-blue-800" : "")}
+                >
+                    Books
+                </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/about' className={({isActive}) => (isActive ? 'text-blue-800' : '')}>
-                        About Us</NavLink>
-
+                <li className="md:ml-6">
+                <NavLink
+                    to="/about"
+                    className={({ isActive }) => (isActive ? "text-blue-800" : "")}
+                >
+                    About Us
+                </NavLink>
                 </li>
-
             </ul>
-        </nav>
-    );
+      </div>
+
+      
+    </nav>
+  );
 };
 
 export default Header;
